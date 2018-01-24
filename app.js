04c5b564e811083a3061ad1d239f9fa744b19205
Ida Englund <ida.englund@chasacademy.se>
2018-01-24 15:39:17 +0100

    Make the card and column drop and draggable

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
-$(function() {
-    function initSort() {
-        $('.column').sortable({
-            connectWith: '.column', 
-            handle: '.list_header'
-        }); 
+$(function(){
+         $('.card').draggable(); 
+         $('.card').droppable();
+         $('.column').draggable(); 
+         $('.column').droppable();  
+    }); 
 
-
-    initSort(); 
-
-    $('body').on('click', '$addNew', function() {
-        var newList = $('.board.clomun:first.list').clone(); 
-        // get the last column
-        var newList =
-         `<div class= "list">
-            <div class = "list_header">
-                Done
-            </div>
-            <ul class="list_cards">
-                <li class= "card">
-                    Card#3  
-                </li>
-            </ul>          
-        </div>
-        `;
-    } 
